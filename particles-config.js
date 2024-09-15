6bcf0e1767986606a485695efea61efca870c75b
cuongismeVN <cskh.cuongisme@gmail.com>
2024-09-15 15:07:54 +0700

    particles-config.js

---
+++ particles-config.js
@@ -0,0 +1,81 @@
+// particles-config.js
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#FF69B4" // Màu hồng đậm
+        },
+        "shape": {
+            "type": "circle", // Sử dụng hình tròn cho các hạt
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.9, // Tăng độ rõ
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 1,
+                "opacity_min": 0.2,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 4, // Tăng kích thước hạt
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 2,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false
+        },
+        "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "bottom",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": false,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": false,
+                "mode": "push"
+            },
+            "resize": true
+        }
+    },
+    "retina_detect": true
+});
